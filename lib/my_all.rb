202c8cd9941f39de1dca94962137d222c5987a41
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-23 01:49:02 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,10 +7,18 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1
   end
+<<<<<<< HEAD
 
+=======
+ 
+>>>>>>> 97490a4f9e983675f2aaacd76a10d5894a02075a
   if block_return_values.include?(false)
     false
   else
     true
   end
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 97490a4f9e983675f2aaacd76a10d5894a02075a
